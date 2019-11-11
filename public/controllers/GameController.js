09fcc85f7d0dc57e3c0b1d377012ec8206e4bf63
josielesgoncalves <josielesgoncalves@gmail.com>
2019-11-11 20:32:56 -0300

    adiciona lista de jogos a view

--- public/controllers/GameController.js
+++ public/controllers/GameController.js
@@ -14,24 +14,21 @@ class GameController {
             for(var i = 0; i < data.length; i++) {    
                 var gameTitle = data[i].title;                
                 gameList = '<div class="game-item">'+ 
-                '<h2>'+ gameTitle +'</h2>';
+                '<h2>'+  data[i].title +'</h2>';
 
                 var games = [];
                 var gameName;
                 var gameImage;
                 for(var j = 0; j < data[i].gameDTOList.length; j++){
-                    gameImage = data[i].gameDTOList[i].mediaDTO.headerImage;
+                    gameImage = data[i].gameDTOList[j].mediaDTO.headerImage;
                     gameName = data[i].gameDTOList[j].name;
                     
-                    gameList = gameList + '<h5>' + gameName + '</h5>'+
-                    '<img src='+ gameImage +'alt="#">' + '</div>';
-                    
-                    document.getElementById(this.id).innerHTML += gameList;                    
+                    gameList = gameList + '<h5>' + data[i].gameDTOList[j].name + '</h5>'+
+                    '<img src='+ data[i].gameDTOList[j].mediaDTO.headerImage +'alt="#">' + '</div>';
                     games.push({gameName, gameImage});                    
                 }
-                this.games.push({gameTitle, games});  
+                document.getElementById(this.id).innerHTML += gameList;                    
             }
-            console.log(this.games);
         })
     }
 }
